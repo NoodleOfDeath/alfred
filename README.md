f149fcc997a09b3e1999d41fe0b83c6b96e1c4d2
NoodleOfDeath <thom@noodleofdeath.com>
2022-02-23 11:21:57 -0500

    minor code optimization for ios

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Just a fun project that allows you to control an iRobot Create 2 from React.js o
 * Acquire an [iRobot Create 2](https://store.irobot.com/default/create-programmable-programmable-robot-irobot-create-2/RC65099.html)
 * Acquire a [Raspberry Pi 4](https://www.amazon.com/gp/product/B07TC2BK1X/ref=ppx_yo_dt_b_asin_title_o09_s00?ie=UTF8&psc=1)
 * A separate machine running Ubuntu 20.04 (A VM will work if you use a Bridge Network connection. All devices need to be on the same local network.)
+* Xcode is optional if you want to run the iOS POC.
 
 ### Setup
 
--- ui/ios/Shared/AlfredMobileApp.swift
+++ ui/ios/Shared/AlfredMobileApp.swift
@@ -9,29 +9,6 @@ import SwiftUI
 
 let APIEndpoint = "http://batcomputer:8000"
 
-extension Dictionary {
-    func percentEncoded() -> Data? {
-        return map { key, value in
-            let escapedKey = "\(key)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
-            let escapedValue = "\(value)".addingPercentEncoding(withAllowedCharacters: .urlQueryValueAllowed) ?? ""
-            return escapedKey + "=" + escapedValue
-        }
-        .joined(separator: "&")
-        .data(using: .utf8)
-    }
-}
-
-extension CharacterSet {
-    static let urlQueryValueAllowed: CharacterSet = {
-        let generalDelimitersToEncode = ":#[]@" // does not include "?" or "/" due to RFC 3986 - Section 3.4
-        let subDelimitersToEncode = "!$&'()*+,;="
-
-        var allowed = CharacterSet.urlQueryAllowed
-        allowed.remove(charactersIn: "\(generalDelimitersToEncode)\(subDelimitersToEncode)")
-        return allowed
-    }()
-}
-
 @main
 struct AlfredMobileApp: App {
     var body: some Scene {
